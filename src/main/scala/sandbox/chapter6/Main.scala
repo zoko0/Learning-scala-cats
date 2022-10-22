be2a9445928405874ecf2cc51a48007572e8e05f
bciulkin <bartosz.ciulkin@gmail.com>
2022-10-22 15:53:21 +0200

    Exercise parallel list..

--- src/main/scala/sandbox/chapter6/Main.scala
+++ src/main/scala/sandbox/chapter6/Main.scala
@@ -3,5 +3,7 @@ package sandbox.chapter6
 //object Main {
 object Main extends App {
 
-
+  // Exercise: Parallel List
+  // Lists do have Parallel instance that puts provided lists together in random manner,
+  // instead of product which does cartesian product
 }
